7992414c889f8a37c1a705a10a791a79e0a40ad1
Seth Vandebrooke <sethvandebrooke@gmail.com>
2020-06-18 08:12:47 -0400

    init commit

---
+++ api/controllers/view-features.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View features',
+
+
+  description: 'Display "Features" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/features'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
